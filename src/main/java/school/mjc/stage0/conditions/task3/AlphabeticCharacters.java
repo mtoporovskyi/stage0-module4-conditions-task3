8005c9f7b2814098f086049f8ec6d7e748b4db7b
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2023-12-08 22:39:00 +0200

    finished stage0-module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+//        a, e, i, o, u, and sometimes y
+        if ((character > 'a' && character < 'z') || (character > 'A' && character < 'Z')) {
+            if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
